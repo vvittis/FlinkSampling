445e3f39fe7f04daecd191cb26b6845526820f49
Vasilis Vittis <vasilisvittis@gmail.com>
2020-02-27 21:47:01 +0200

    Updating KafkaProducer

---
+++ ECE622-SingleProducer/src/main/java/Producer/ECE622/App.java
@@ -0,0 +1,13 @@
+package Producer.ECE622;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
